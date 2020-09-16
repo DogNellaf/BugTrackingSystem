3d985033b39975d68319f08ca457cd774cdb3bc7
FallenGod <dans-host@ya.ru>
2020-09-16 16:08:11 +0300

    Uploaded base of project
    Base new project in Visual studio
    3 new classes of data (User, Task, Project)

---
+++ Task.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackingSystem
+{
+    class Task
+    {
+        /*
+         * текущий проект
+         * тема
+         * тип
+         * приоритет
+         * исполнитель
+         * описание
+         */
+
+        private Project currentProject;
+        private User contractor;
+
+        private string topic;
+        private string type;
+        private string priority;
+        private string description;
+
+        public Task(Project project, User user, string[] args = null)
+        {
+            currentProject = project;
+            contractor = user;
+            if (args != null)
+            {
+                try
+                {
+                    topic = args[0];
+                    type = args[1];
+                    priority = args[2];
+                    description = args[3];
+                }
+                catch { }
+                
+            }
+        }
+
+    }
+}
