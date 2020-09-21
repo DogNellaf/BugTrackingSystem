4e63da4edaafab8493b8b040661a305e4dc7de83
FallenGod <dans-host@ya.ru>
2020-09-21 14:14:07 +0300

    Uploaded SqlLite library

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,6 +28,7 @@ namespace BugTrackingSystem
 
         private void button_load_Click(object sender, RoutedEventArgs e)
         {
+            //получаем путь до папки
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
@@ -34,11 +36,19 @@ namespace BugTrackingSystem
                 Console.WriteLine(FilePath);
             }
 
+            //если файл существует
+            if (File.Exists(FilePath))
+            {
+                SQLiteConnection databaseConnection = 
+                    new SQLiteConnection("Data Source=Base;Version=3;");
+                databaseConnection.Open();
+            }
+
         }
 
         private void button_usereditor_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void button_taskeditor_Click(object sender, RoutedEventArgs e)
